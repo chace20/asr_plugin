8cf303a02da8452f03e1cb686eec5292b34ed6b8
chace <chao9420@gmail.com>
2024-05-22 21:36:42 +0800

    Init commit

---
+++ android/src/main/java/com/tencent/cloud/asr/plugin/asr_plugin/ASRControllerDataSource.java
@@ -0,0 +1,82 @@
+package com.tencent.cloud.asr.plugin.asr_plugin;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.tencent.aai.audio.data.PcmAudioDataSource;
+import com.tencent.aai.exception.ClientException;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+
+import io.flutter.plugin.common.MethodChannel;
+
+public class ASRControllerDataSource implements PcmAudioDataSource {
+    private int _id = 0;
+    private MethodChannel _channel;
+
+    ASRControllerDataSource(int id, MethodChannel channel){
+        _id = id;
+        _channel = channel;
+    }
+
+    @Override
+    public int read(short[] audioPcmData, int length) {
+        Map<String, Object> args = new HashMap<>();
+        args.put("id", _id);
+        args.put("size", length * 2);
+        CountDownLatch count = new CountDownLatch(1);
+        final int[] real_len = {0};
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                _channel.invokeMethod("read", args, new MethodChannel.Result() {
+                    @Override
+                    public void success(@Nullable Object result) {
+                        count.countDown();
+                        byte[] content = (byte[]) result;
+                        ByteBuffer.wrap(content).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(audioPcmData);
+                        real_len[0] = length;
+                    }
+
+                    @Override
+                    public void error(@NonNull String errorCode, @Nullable String errorMessage, @Nullable Object errorDetails) {
+                        count.countDown();
+                    }
+
+                    @Override
+                    public void notImplemented() {
+                        throw new RuntimeException();
+                    }
+                });
+            }
+        });
+        try {
+            count.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return real_len[0];
+    }
+
+    @Override
+    public void start() throws ClientException {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public boolean isSetSaveAudioRecordFiles() {
+        return false;
+    }
+}
